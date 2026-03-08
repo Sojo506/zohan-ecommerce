3a758ed5171062dd2c6ecb3273385838facd1188
Sojo506 <fsojodev@gmail.com>
2026-03-08 11:49:46 -0600

    modify query when register() user

--- app/controllers/AuthController.php
+++ app/controllers/AuthController.php
@@ -160,7 +160,7 @@ class AuthController extends Controller
                 ':nom' => $nombre,
                 ':ap1' => $ap1,
                 ':ap2' => ($ap2 === '' ? null : $ap2),
-                ':tipoUsuario' => 1,
+                ':tipoUsuario' => 2, // cliente
                 ':estado' => $this->ESTADO_PENDIENTE,
             ]);
 
