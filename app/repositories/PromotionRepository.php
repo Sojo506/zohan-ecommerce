32272ee3fabda7f8e40532d2e03b8e9aeb389ba0
Sojo506 <fsojodev@gmail.com>
2026-03-10 23:16:10 -0600

    add PromotionRepository

---
+++ app/repositories/PromotionRepository.php
@@ -0,0 +1,129 @@
+<?php
+
+class PromotionRepository
+{
+    private PDO $db;
+
+    public function __construct()
+    {
+        $this->db = Database::connection();
+    }
+
+    public function all()
+    {
+        $sql = "SELECT *
+                FROM PROMOCION_TB
+                WHERE ID_ESTADO = 1
+                ORDER BY FECHA_INICIO DESC";
+
+        return $this->db->query($sql)->fetchAll();
+    }
+
+    public function find($id)
+    {
+        $stmt = $this->db->prepare("
+            SELECT * FROM PROMOCION_TB
+            WHERE ID_PROMOCION = :id
+        ");
+
+        $stmt->execute([':id' => $id]);
+
+        return $stmt->fetch();
+    }
+
+    public function create($data)
+    {
+        $stmt = $this->db->prepare("
+            INSERT INTO PROMOCION_TB
+            (NOMBRE,DESCRIPCION,PORCENTAJE,FECHA_INICIO,FECHA_FIN,ID_ESTADO)
+            VALUES(:name,:desc,:percent,:start,:end,1)
+        ");
+
+        $stmt->execute([
+            ':name' => $data['name'],
+            ':desc' => $data['description'],
+            ':percent' => $data['percent'],
+            ':start' => $data['start'],
+            ':end' => $data['end']
+        ]);
+    }
+
+    public function update($id, $data)
+    {
+        $stmt = $this->db->prepare("
+            UPDATE PROMOCION_TB
+            SET
+                NOMBRE=:name,
+                DESCRIPCION=:desc,
+                PORCENTAJE=:percent,
+                FECHA_INICIO=:start,
+                FECHA_FIN=:end
+            WHERE ID_PROMOCION=:id
+        ");
+
+        $stmt->execute([
+            ':name' => $data['name'],
+            ':desc' => $data['description'],
+            ':percent' => $data['percent'],
+            ':start' => $data['start'],
+            ':end' => $data['end'],
+            ':id' => $id
+        ]);
+    }
+
+    public function delete($id)
+    {
+        $stmt = $this->db->prepare("
+            UPDATE PROMOCION_TB
+            SET ID_ESTADO = 2
+            WHERE ID_PROMOCION = :id
+        ");
+
+        $stmt->execute([':id' => $id]);
+    }
+
+    public function assignProduct($promoId, $productId)
+    {
+        $stmt = $this->db->prepare("
+            INSERT INTO PROMOCION_PRODUCTO_TB
+            (ID_PROMOCION,ID_PRODUCTO,ID_ESTADO)
+            VALUES(:promo,:product,1)
+        ");
+
+        $stmt->execute([
+            ':promo' => $promoId,
+            ':product' => $productId
+        ]);
+    }
+
+    public function products($promoId)
+    {
+        $stmt = $this->db->prepare("
+            SELECT P.ID_PRODUCTO,P.NOMBRE
+            FROM PROMOCION_PRODUCTO_TB PP
+            JOIN PRODUCTO_TB P
+            ON P.ID_PRODUCTO = PP.ID_PRODUCTO
+            WHERE PP.ID_PROMOCION = :id
+            AND PP.ID_ESTADO = 1
+        ");
+
+        $stmt->execute([':id' => $promoId]);
+
+        return $stmt->fetchAll();
+    }
+
+    public function removeProduct($promoId, $productId)
+    {
+        $stmt = $this->db->prepare("
+        UPDATE PROMOCION_PRODUCTO_TB
+        SET ID_ESTADO = 2
+        WHERE ID_PROMOCION = :promo
+        AND ID_PRODUCTO = :product
+    ");
+
+        $stmt->execute([
+            ':promo' => $promoId,
+            ':product' => $productId
+        ]);
+    }
+}
