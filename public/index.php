123452ff3fba05fa5499f8c17f2608f1e242800a
alexcm06 <camposmarinalexander6@gmail.com>
2026-03-13 18:04:06 -0600

    add routes for password change functionality in profile controller

--- public/index.php
+++ public/index.php
@@ -176,6 +176,10 @@ $router->get('/admin/audit', 'AuditAdminController@index');
 $router->get('/profile', 'ProfileController@index');
 $router->get('/editProfile', 'ProfileController@edit');
 $router->post('/update', 'ProfileController@update');
+$router->get('/sendPasswordOtp', 'ProfileController@sendPasswordOtp');
+$router->get('/changePassword', 'ProfileController@changePassword');
+$router->post('/changePassword', 'ProfileController@updatePassword');
+
 
 /* RUTAS AUTENTICACIÓN  */
 
