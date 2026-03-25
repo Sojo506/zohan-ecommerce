3aa90d01d53968c90aedd399d5e5853377162e5e
Carlos-Santos2004 <cjimenez80173@ufide.ac.cr>
2026-03-25 16:39:25 -0600

    fix: badge carrito en inicio

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once __DIR__ . '/../models/ProductModel.php';
+require_once __DIR__ . '/../repositories/ProductRepository.php';
 
 class HomeController extends Controller
 {
@@ -9,10 +10,18 @@ class HomeController extends Controller
         $productModel = new ProductModel();
         $featuredProducts = $productModel->obtenerProductosDestacadosSemana(8);
         $promoProducts = $productModel->obtenerProductosEnPromocion(8);
+        $repository = new ProductRepository($productModel);
+        $cart = $repository->sanitizeCart($_SESSION['cart'] ?? []);
+        if (empty($cart) && isset($_SESSION['user']['id_cuenta'])) {
+            $cart = $repository->fetchCartForAccount((int)$_SESSION['user']['id_cuenta']);
+        }
+        $_SESSION['cart'] = $cart;
+        $cartCount = $repository->countCart($cart);
 
         $this->view('home/index', [
             'featuredProducts' => $featuredProducts,
-            'promoProducts' => $promoProducts
+            'promoProducts' => $promoProducts,
+            'cartCount' => $cartCount
         ]);
     }
 }
